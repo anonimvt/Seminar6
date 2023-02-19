85476a0f2264bf08025430c8e2f7803240677a01
anonimvt <rinat.madyarov.08@gmail.com>
2023-02-19 12:33:19 +0400

    Commit for Seminar6

---
+++ Task41/Task41/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int n = InputInt("Введите количество чисел: ");
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine(count);
+
+
+int InputInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+double b1 = Input("Введите коэффициент 1-ой прямой: ");
+double k1 = Input("Введите свободный член 1-ой прямой: ");
+double b2 = Input("Введите коэффициент 2-ой прямой: ");
+double k2 = Input("Введите свободный член 2-ой прямой: ");
+
+double x = (b1 - b2) / (k1 - k2);
+double y = k2 * x + b2; 
+
+Console.WriteLine($"({Math.Round(x, 2)}, {Math.Round(y, 2)})");
+
+double Input(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
